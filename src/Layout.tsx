e8f2b8fb6629bfae83b11cc90572c4b2609a99d0
nirwairkumar <nkchaudhary431@gmail.com>
2026-01-02 02:44:00 +0530

    Remove footer from test intro page

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -29,8 +29,8 @@ export default function Layout() {
         }
     }, [user, navigate, location.pathname]);
     // Hide navbar only on live test page (/test/:id)
-    // Note: /test-intro/:id starts with /test-intro so it won't match /test/
-    const isLiveTestPage = location.pathname.startsWith('/test/');
+    // Also hiding on /test-intro/:id as requested
+    const isLiveTestPage = location.pathname.startsWith('/test/') || location.pathname.startsWith('/test-intro/');
 
     return (
         <div className="min-h-screen bg-slate-50 flex flex-col">
